a3ab33047027004309bfa0438b7b30b42d8f14ee
Umang Srivastava <umang.srivastava.ckc@gmail.com>
2023-11-23 17:20:02 +0530

    added min cost of climbing stairs

---
+++ Recursion/4. Min Cost Climbing Stairs/min_cost_climbing_stairs.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/min-cost-climbing-stairs
+# (TLE solutions - not the most optimised solutions)
+
+# Bottom up approach
+# Actual problem: Find min cost to reach top of stairs
+# Sub problem: find min cost to reach n+1 or n+2 steps
+# Recurrence relation: f(n)=min(cost[n]+f(n+1),cost[n+1]+f(n+2))
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        return min(self.costToClimbStairs(0,cost),self.costToClimbStairs(1,cost))  # since we can start from either step 0 or step 1
+    def costToClimbStairs(self,currentStep: int,cost: List[int])-> int:
+        if currentStep>= len(cost):  # base condition
+            return 0
+        return cost[currentStep]+min(self.costToClimbStairs(currentStep+1,cost),self.costToClimbStairs(currentStep+2,cost))
+
+
+
+
+# Top down approach
+# Recurrence relation: f(n)=min(cost[n]+f(n-1),cost[n-1]+f(n-2))
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        return min(self.costToClimbStairs(len(cost)-1,cost),self.costToClimbStairs(len(cost)-2,cost)) 
+    def costToClimbStairs(self,currentStep: int,cost: List[int])-> int:
+        if currentStep<=1:  # base condition
+            return cost[currentStep]
+        return cost[currentStep]+min(self.costToClimbStairs(currentStep-1,cost),self.costToClimbStairs(currentStep-2,cost))
