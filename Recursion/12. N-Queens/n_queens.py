8a3a5a8179fe707655c59e689702031c50328a10
Umang Srivastava <umang.srivastava.ckc@gmail.com>
2023-11-27 09:56:15 +0530

    Added n-queens solution

--- Recursion/12. N-Queens/n_queens.py
+++ Recursion/12. N-Queens/n_queens.py
@@ -3,6 +3,7 @@
 # Actual problem: can we place queens at n rows/cols
 # sub-problem: can we place a queen at n-1 row/col
 
+# Column traversal approach
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
@@ -38,3 +39,39 @@ class Solution:
             r+=1
             c-=1        
         return True
+
+# Row traversal
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+            result,board=[],[["."]*n for i in range(n)]    
+            self.placeQueen(0,n,board,result)
+            return result
+
+    def placeQueen(self,row: int,n:int,board: List[str], result:List[List[str]]):
+            if row==n:
+                copy=["".join(col) for col in board]
+                result.append(copy)
+                return
+            for col in range(0,n):
+                if self.isValid(row,col,n,board):
+                    board[row][col]='Q'                
+                    self.placeQueen(row+1,n,board,result)
+                    board[row][col]='.'
+
+    def isValid(self,row:int,col:int,n:int,board:List[str])->bool:
+            for r in range(0,row):
+                if board[r][col]=='Q':
+                    return False
+            r,c=row,col
+            while r>=0 and c>=0:
+                if board[r][c]=='Q':
+                    return False
+                r-=1
+                c-=1
+            r,c=row,col
+            while r>=0 and c<n:
+                if board[r][c]=='Q':
+                    return False
+                r-=1
+                c+=1        
+            return True
