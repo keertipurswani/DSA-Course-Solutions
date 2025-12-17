c1f7efb1c6103a326f5e31cd2b39b0e5854d3522
Keerti Purswani <22031471+keertipurswani@users.noreply.github.com>
2025-12-17 20:26:55 +0530

    Add Python Code

---
+++ BST/1. Insert in BST/Python.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/insert-into-a-binary-search-tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None:
+            return TreeNode(val)
+
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:   # val < root.val
+            root.left = self.insertIntoBST(root.left, val)
+
+        return root
